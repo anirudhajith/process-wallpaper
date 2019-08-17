82edf8427f746f9861ebeb3df025089d389c918e
Anirudh Ajith <anirudh.ajith@gmail.com>
2019-08-18 01:41:02 +0530

    Used regex to clean tree lines

--- parsePsTree.py
+++ parsePsTree.py
@@ -1,1 +1,10 @@
+import re
+
 treeFile = open("tree", "r")
+
+jsonString = "["
+
+for line in treeFile:
+    line = re.sub(r"[\s - \|]", "", line, flags=re.UNICODE)
+    line = re.sub(r"\-\+\-", "+", line, flags=re.UNICODE)
+    print(line)
