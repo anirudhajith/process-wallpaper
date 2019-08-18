bb4dfd6fda157cd91504002cc418d84105176403
Anirudh Ajith <anirudh.ajith@gmail.com>
2019-08-18 19:44:01 +0530

    Bug-fix, improved updateWallpaper.sh

--- updateWallpaper.sh
+++ updateWallpaper.sh
@@ -1,2 +1,4 @@
 top -b -n 1 > top.out
-python3 generateWordCloud.py
+python3 generateWallpaper.py
+WALLPAPER_PATH="file://$(pwd)/wallpaper.png"
+gsettings set org.gnome.desktop.background picture-uri $WALLPAPER_PATH
