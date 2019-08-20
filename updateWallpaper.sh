f5e5e5da02ecc81bda8bfa1d5c86f766393269ac
zaphbbrox <hut_punk1991@gmx.de>
2019-08-21 01:01:32 +0200

    changed updateWallpaper.sh for cron usage

--- updateWallpaper.sh
+++ updateWallpaper.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-export DISPLAY=:1
-top -b -n 1 -i > top.out
-nice python3 generateWallpaper.py
+# Usual path where 'top', 'python3', 'gsettings' etc., reside
+PATH=$PATH:/usr/bin
+
+# Get the full directory name of the script no matter where it is being called from
+APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Save top's output to our app directory
+top -b -n 1 -i -w 512 > $APP_DIR/top.out
+
+# This is important!
+cd $APP_DIR
+
+python3 $APP_DIR/generateWallpaper.py
+cp wallpaper_new.png wallpaper.png
