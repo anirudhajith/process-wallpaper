717e945dbc6f9a2a403fa042f94e059efc3a860d
Anirudh Ajith <anirudh.ajith@gmail.com>
2019-08-20 19:23:38 +0530

    Switched the order in which gsettings and feh are tested

--- setWallpaper.sh
+++ setWallpaper.sh
@@ -2,13 +2,13 @@
 WALLPAPER_PATH="$(pwd)/wallpaper.png"
 
 
-if command -v feh
-then
-  feh --bg-fill $WALLPAPER_PATH
-elif command -v gsettings
+if command -v gsettings
 then
   gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER_PATH"
+elif command -v feh
+then
+  feh --bg-fill $WALLPAPER_PATH
 else
-  echo "Don't know how to set wallpaper on your system. You need to manually set your wallpaper to ${WALLPAPER_PATH} ."
+  echo "ERROR: Unable to automatically set wallpaper on your system. Manually set your wallpaper to ${WALLPAPER_PATH}."
 fi
 
