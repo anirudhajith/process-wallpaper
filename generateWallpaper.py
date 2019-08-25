f5a1a6d07642b0c8bd574f830f3f8d12db2f542e
Federico Muzzo <16427849+mzzfederico@users.noreply.github.com>
2019-08-25 14:04:01 +0200

    reorganized code and fixed setup for Bash for Windows

--- generateWallpaper.py
+++ generateWallpaper.py
@@ -7,55 +7,88 @@ from sys import platform
 from PIL import Image
 from wordcloud import WordCloud
 
-resourceDict = dict()
 
-for proc in psutil.process_iter(attrs=['name', 'cpu_percent', 'memory_percent']):
-    try:
-        if os.name != 'nt':
-            name = proc.name()
-        else: 
-            executable = proc.name()
-            name, extension = os.path.splitext(executable)
-        if name != "Python":
-            relevancy = (proc.cpu_percent() ** 2 + proc.memory_percent() ** 2) ** 0.5
-            if name in resourceDict:
-                resourceDict[name] = resourceDict[name] + relevancy
+
+def init_screen_sizes(configJSON):
+    width, height = None, None
+    if platform == "linux" or platform == "linux2":
+        try:
+            width, height = (
+                (os.popen("xrandr | grep '*'").read()).split()[0]).split("x")
+        except:
+            pass
+    elif platform == "darwin":
+        try:
+            results = str(subprocess.Popen(
+                ['system_profiler SPDisplaysDataType'], stdout=subprocess.PIPE, shell=True).communicate()[0])
+            res = re.search('Resolution: \d* x \d*',
+                            results).group(0).split(' ')
+            width, height = res[1], res[3]
+        except:
+            pass
+    else:
+        width = int(configJSON["resolution"]["width"])
+        height = int(configJSON["resolution"]["height"])
+
+    return (width, height)
+
+def borderize(width, height, margin, background):
+    cloud = Image.open("wallpaper.png")
+    wallpaper = Image.new(
+        'RGB',
+        (width, height),
+        background
+    )
+    wallpaper.paste(cloud, (margin, margin))
+    wallpaper.save("wallpaper.png")
+
+def get_resources():
+    resourceDict = dict()
+    for proc in psutil.process_iter(attrs=['name', 'cpu_percent', 'memory_percent']):
+        try:
+            if os.name != 'nt':
+                name = proc.name()
             else:
-                resourceDict[name] = relevancy
-    except:
-        pass
+                executable = proc.name()
+                name, extension = os.path.splitext(executable)
+            if name != "Python":
+                relevancy = (proc.cpu_percent() ** 2 +
+                             proc.memory_percent() ** 2) ** 0.5
+                if name in resourceDict:
+                    resourceDict[name] = resourceDict[name] + relevancy
+                else:
+                    resourceDict[name] = relevancy
+        except:
+            pass
+    return resourceDict
 
-width, height = None, None
+def dump_screen_sizes(configJSON, screen_sizes):
+    width = screen_sizes[0]
+    height = screen_sizes[1]
+    if height and width:
+        configJSON["resolution"]["width"] = int(width)
+        configJSON["resolution"]["height"] = int(height)
+        with open('config.json', 'w') as f:
+            json.dump(configJSON, f, indent=4)
 
-if platform == "linux" or platform == "linux2":
-    try:
-        width,height = ((os.popen("xrandr | grep '*'").read()).split()[0]).split("x")
-    except:
-        pass
-elif platform == "darwin":
-    try:
-        results = str(subprocess.Popen(['system_profiler SPDisplaysDataType'],stdout=subprocess.PIPE, shell=True).communicate()[0])
-        res = re.search('Resolution: \d* x \d*', results).group(0).split(' ')
-        width, height = res[1], res[3]
-    except:
-        pass
+
 
 configJSON = json.loads(open("config.json", "r").read())
+margin = int(configJSON["wordcloud"]["margin"])
+background = configJSON["wordcloud"]["background"]
 
-if height and width:
-    configJSON["resolution"]["width"] = int(width)
-    configJSON["resolution"]["height"] = int(height)
-    with open('config.json', 'w') as f:
-        json.dump(configJSON, f, indent=4)
+screen_sizes = init_screen_sizes(configJSON)
+dump_screen_sizes(configJSON, screen_sizes)
 
+resourceDict = get_resources()
+
+width = screen_sizes[0]
+height = screen_sizes[1]
 cloud = WordCloud(
-    background_color = configJSON["wordcloud"]["background"],
-    width = int(configJSON["resolution"]["width"] - 2 * configJSON["wordcloud"]["margin"]),
-    height = int(configJSON["resolution"]["height"] - 2 * configJSON["wordcloud"]["margin"])
+    background_color=background,
+    width=int(width - (2 * margin)),
+    height=int(height - (2 * margin))
 ).generate_from_frequencies(resourceDict)
-
 cloud.to_file("wallpaper.png")
-wallpaper = Image.open("wallpaper.png")
-wallpaper = Image.new('RGB', (configJSON["resolution"]["width"], configJSON["resolution"]["height"]), configJSON["wordcloud"]["background"])
-wallpaper.paste(cloud, None)
-wallpaper.save("wallpaper.png")
+
+borderize(width, height, margin, background)
--- setup.sh
+++ setup.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 if [[ "$OSTYPE" =~ ^msys ]]; then
-  ## Fixes the installer on Bash for Windows
-  alias python3='py -3'
+    ## Fixes the installer on Bash for Windows
+    alias python3='py -3'
+    echo "Checking Python dependencies..."
+    pillow="$(py -3 -c 'import pkgutil; print(1 if pkgutil.find_loader("PIL") else 0)')"
+    wordcloud="$(py -3 -c 'import pkgutil; print(1 if pkgutil.find_loader("wordcloud") else 0)')"
+    matplotlib="$(py -3 -c 'import pkgutil; print(1 if pkgutil.find_loader("matplotlib") else 0)')"
+    psutil="$(py -3 -c 'import pkgutil; print(1 if pkgutil.find_loader("psutil") else 0)')"
+else
+    echo "Checking Python dependencies..."
+    pillow="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("PIL") else 0)')"
+    wordcloud="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("wordcloud") else 0)')"
+    matplotlib="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("matplotlib") else 0)')"
+    psutil="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("psutil") else 0)')"
 fi
-
-echo "Installing Python dependencies..."
-
-pillow="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("PIL") else 0)')"
-wordcloud="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("wordcloud") else 0)')"
-matplotlib="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("matplotlib") else 0)')"
-psutil="$(python3 -c 'import pkgutil; print(1 if pkgutil.find_loader("psutil") else 0)')"
 
 if [[ $pillow == 0 ]]; then
-echo "Installing Pillow..."
-pip3 install --user Pillow
+    echo "Installing Pillow..."
+    pip3 install --user Pillow
 fi
 if [[ $wordcloud == 0 ]]; then
-echo "Installing wordcloud..."
-pip3 install --user wordcloud
+    echo "Installing wordcloud..."
+    pip3 install --user wordcloud
 fi
 if [[ $matplotlib == 0 ]]; then
-echo "Installing matplotlib..."
-pip3 install --user matplotlib
+    echo "Installing matplotlib..."
+    pip3 install --user matplotlib
 fi
 if [[ $psutil == 0 ]]; then
-echo "Installing psutil..."
-pip3 install --user psutil
+    echo "Installing psutil..."
+    pip3 install --user psutil
 fi
 
 echo "Creating wallpaper..."
