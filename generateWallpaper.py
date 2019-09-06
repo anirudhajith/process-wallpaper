837fa22a4e7bf2898a8a124736937b36e8eb44a5
David Luo <me@dsluo.dev>
2019-09-06 18:10:08 -0400

    code cleanup

--- generateWallpaper.py
+++ generateWallpaper.py
@@ -1,12 +1,11 @@
-import re
+import json
+import os
 import time
 from collections import defaultdict
 
+import psutil
 from PIL import Image
 from wordcloud import WordCloud
-import json
-import os
-import psutil
 
 # get all process names with cpu and memory usage
 all_processes = defaultdict(lambda: {'cpu_percent': 0, 'memory_percent': 0})
@@ -17,29 +16,16 @@ time.sleep(0.2)  # wait so that we're not measuring cpu/memory usage of the time
 
 for proc in psutil.process_iter():
     details = proc.as_dict(attrs=['name', 'cpu_percent', 'memory_percent'])
-    
-    if details['name'].count("/") > 0:
-        details['name'] = details['name'].split("/")[0]
-    
-    if details['cpu_percent'] == None:
-        details['cpu_percent'] = 0.0
-    
-    if details['memory_percent'] == None:
-        details['memory_percent'] = 0.0
-    
-    if details['name'] not in all_processes:
-        all_processes[details['name']] = details
-    else:
-        all_processes[details['name']]['memory_percent'] += details['memory_percent']
-        all_processes[details['name']]['cpu_percent'] += details['cpu_percent']
 
+    name = details['name'].split("/")[0]
+    all_processes[name]['cpu_percent'] += details['cpu_percent'] or 0
+    all_processes[name]['memory_percent'] += details['memory_percent'] or 0
 
 # generate a single real number metric for resource-intensity
-resourceDict = {}
+frequencies = {}
 
-for process_name in all_processes.keys():
-    resourceDict[process_name] = (all_processes[process_name]['cpu_percent'] ** 2 + all_processes[process_name]['memory_percent'] ** 2 + 1) ** 0.5
-
+for name, resources in all_processes.items():
+    frequencies[name] = (resources['cpu_percent'] ** 2 + resources['memory_percent'] ** 2 + 1) ** 0.5
 
 # create wallpaper
 width, height = None, None
@@ -50,27 +36,27 @@ try:
 except:
     pass
 
-configJSON = json.loads(open("config.json", "r").read())
+config = json.loads(open("config.json", "r").read())
 
 if not width or not height:
-    width = configJSON['resolution']['width']
-    height = configJSON['resolution']['height']
+    width = config['resolution']['width']
+    height = config['resolution']['height']
 
 wc = WordCloud(
-    background_color=configJSON["wordcloud"]["background"],
-    width=width - 2 * int(configJSON["wordcloud"]["margin"]),
-    height=height - 2 * int(configJSON["wordcloud"]["margin"])
-).generate_from_frequencies(resourceDict)
+    background_color=config["wordcloud"]["background"],
+    width=width - 2 * int(config["wordcloud"]["margin"]),
+    height=height - 2 * int(config["wordcloud"]["margin"])
+).generate_from_frequencies(frequencies)
 
 wc.to_file('wc.png')
 
 wordcloud = Image.open("wc.png")
-wallpaper = Image.new('RGB', (width, height), configJSON["wordcloud"]["background"])
+wallpaper = Image.new('RGB', (width, height), config["wordcloud"]["background"])
 wallpaper.paste(
     wordcloud,
     (
-        configJSON["wordcloud"]["margin"],
-        configJSON["wordcloud"]["margin"]
+        config["wordcloud"]["margin"],
+        config["wordcloud"]["margin"]
     )
 )
 wallpaper.save("wallpaper.png")
